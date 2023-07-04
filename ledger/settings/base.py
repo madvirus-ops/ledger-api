96e6f8b176c7ff2d7bb8cc895b1e432a43d8654c
madvirus-ops <edwinayabie1@gmail.com>
2023-07-04 17:46:18 +0100

    fix model error

--- ledger/settings/base.py
+++ ledger/settings/base.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+AUTH_USER_MODEL = 'users.User'
 
 TREBLLE_HIDDEN_KEYS=['id']
 
